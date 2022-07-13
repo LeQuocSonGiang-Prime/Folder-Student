355554e93bd2ef4b3fd84ab718e441cb7bf56d7f
lequocsongiang-prime <gianglqs07@gmail.com>
2022-07-13 11:42:48 +0700

    complete FILE

--- laptrinhmang/workspace/FileClass/src/bai23/Test.java
+++ laptrinhmang/workspace/FileClass/src/bai23/Test.java
@@ -1,14 +1,26 @@
 package bai23;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 
 public class Test {
 	public static void main(String[] args) {
-		File dir = new File("G:\\tai_lieu_cac_mon_hoc\\laptrinhmang");
+		File dir = new File("G:\\tai_lieu_cac_mon_hoc\\laptrinhmang\\sample-data\\main\\26a4cd00c30a02545b1b.jpg");
 //		System.out.println(dir.getName().substring(0, dir.getName().lastIndexOf('.')));
 		
 //		String s = "abcd*";
 //		System.out.println(s.substring(0,s.length()-1));
-		System.out.println(dir.length());
+		//System.out.println(dir.toPath());
+		String path="G:\\tai_lieu_cac_mon_hoc\\laptrinhmang\\sample-data\\test";
+		System.out.println(path+'\\'+dir.getName());
+		try {
+			Files.copy(new File("G:\\tai_lieu_cac_mon_hoc\\laptrinhmang\\sample-data\\main\\26a4cd00c30a02545b1b.jpg").toPath(),new File("G:\\tai_lieu_cac_mon_hoc\\laptrinhmang\\sample-data\\test\\a\\scgs.jpg").toPath());
+			System.out.println("djikshd");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
--- laptrinhmang/workspace/FileClass/src/bai7/Copy.java
+++ laptrinhmang/workspace/FileClass/src/bai7/Copy.java
@@ -1,25 +1,80 @@
 package bai7;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 
 public class Copy {
 
-	
-	public static void copyAll(String sDir, String dDir, String... ext) {
+	public static void copyAll(String sDir, String dDir, String pathParent, String... ext) {
 		File dir = new File(sDir);
-		if(!dir.exists())
-			return ;
-		if(dir.isFile()) {
-			for(String i : ext) {
-				if(dir.getName().substring(dir.getName().lastIndexOf(".")+1, dir.getName().length()).equals(i)) {
-					
+		if (!dir.exists())
+			return;
+		if (dir.isFile()) {
+			for (String i : ext) {
+				if (dir.getName().substring(dir.getName().lastIndexOf(".") + 1, dir.getName().length()).equals(i)) {
+					try {
+						Files.copy(dir.toPath(), new File(dDir + pathParent + "\\" + dir.getName()).toPath(),
+								StandardCopyOption.REPLACE_EXISTING);
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
 				}
 			}
 		}
-		File[] listFile = dir.listFiles();
-		for(File i: listFile) {
-			
+		if (dir.isDirectory()) {
+			if (pathParent.length() == 0) {
+				pathParent += "\\";
+			} else if (pathParent.equals("\\")) {
+				pathParent += dir.getName();
+			} else {
+				pathParent += "\\" + dir.getName();
+			}
+			File newDir = new File(dDir + pathParent);
+			if (!newDir.exists())
+				newDir.mkdirs();
+			File[] listFile = dir.listFiles();
+			for (File f : listFile) {
+				copyAll(f.getAbsolutePath(), dDir, pathParent, ext);
+			}
 		}
 	}
-	
+
+	public static void copyAll2(String sDir, String dDir, String... ext) {
+		File dir = new File(sDir);
+		if (!dir.exists())
+			return;
+		File newDir = new File(dDir);
+		if (!newDir.exists())
+			newDir.mkdirs();
+		if (dir.isFile()) {
+			for (String i : ext) {
+				if (dir.getName().substring(dir.getName().lastIndexOf(".") + 1, dir.getName().length()).equals(i)) {
+					try {
+						Files.copy(dir.toPath(), new File(dDir + "\\" + dir.getName()).toPath());
+						System.out.println("csjh");
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+		if (dir.isDirectory()) {
+			File[] listFile = dir.listFiles();
+			for (File f : listFile) {
+				copyAll2(f.getAbsolutePath(), dDir,ext);
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+//		Copy.copyAll("G:\\tai_lieu_cac_mon_hoc\\laptrinhmang\\sample-data\\test",
+//				"G:\\tai_lieu_cac_mon_hoc\\laptrinhmang\\sample-data\\test1", "", "txt", "jpg");
+		Copy.copyAll2("G:\\tai_lieu_cac_mon_hoc\\laptrinhmang\\sample-data\\main",
+				"G:\\tai_lieu_cac_mon_hoc\\laptrinhmang\\sample-data\\test2", "txt", "jpg");
+	}
+
 }
